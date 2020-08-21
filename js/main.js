70901ca8fb61e5330482ecb0fc18306839724b68
Roman <65417719+RomanBaukin@users.noreply.github.com>
2020-08-21 22:19:43 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let mySwiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".project-pagination",
+    bulletClass: "projects-bullet",
+    bulletActiveClass: "projects-bullet-active",
+    clickable: true,
+  },
+});
